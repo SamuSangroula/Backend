b0c96bb3b1b0ed0142fc016bf4f1f8951e3b4278
SamuSangroula <samusangroula5@gmail.com>
2026-06-25 01:12:47 +0545

    "Added forgot password feature"

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 mongoose.connect("mongodb://localhost:27017/myDatabase")
 const e = express()
  
 e.listen(4000)
 console.log("Hi")
 
-e.post("/login", function hi(req, res) {
-  res.send("loggedin successfully");
-});
 
 e.use(express.json());
 
 const userSchema = new mongoose.Schema({
 email: String,
 password: String,
+
+resetToken: String,
+resetTokenexpire: Date,
+
 });
 
 const User = mongoose.model("User", userSchema);
 
 // Register
-e.post("/register", async (req, res) => {
+e.post("/register", async function (req, res) {
 const { email, password } = req.body;
 
 const hashedPassword = await bcrypt.hash(password, 10);
@@ -37,7 +39,7 @@ res.send("Registered Successfully");
 });
 
 // Login
-e.post("/login", async (req, res) => {
+e.post("/login", async function (req, res) {
 const { email, password } = req.body;
 
 const user = await User.findOne({ email });
@@ -56,6 +58,20 @@ res.send("Login Successful");
 });
 
 // Forgot Password
-e.post("/forgot-password", (req, res) => {
-res.send("Reset password link sent");
+e.post("/forgot-password", async function (req, res) {
+  const {email} = req.body;
+  const user = await User.findOne({email});
+   if(!user){
+    return res.send("User not found");
+   }
+   const token = crypto.randomBytes(32).toString("hex");
+   user.resetToken = token;
+   user.resetTokenexpire = Date.now() +10 * 60 * 1000; // 1 hour
+   await user.save();
+
+res.send({
+message: "Password reset token generated",
+token: token
+})
 });
+
